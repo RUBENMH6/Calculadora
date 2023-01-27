6a30814be2780e1295e1a206425e31d985dfcefa
irubenmh <alumar1999@ieselcaminas.org>
2023-01-27 16:26:26 +0100

    Intento numero 2

---
+++ src/main/java/org/ieselcaminas/calculadora/Operators.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package org.ieselcaminas.calculadora;
+
+/**
+ *
+ * @author alu10211999
+ */
+public enum Operators {
+    NONE, ADD, SUB, MULT, DIV
+}
